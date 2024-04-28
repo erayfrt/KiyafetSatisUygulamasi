ff00022b04e2674bab9dabcd8692fc6c2abff148
erayfrt <95634384+erayfrt@users.noreply.github.com>
2024-04-29 02:37:22 +0300

    Xml'de stringler düzenlendi

---
+++ app/build/generated/hilt/component_sources/debug/dagger/hilt/internal/aggregatedroot/codegen/_com_notes_app_NotesHiltApp.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.notes.app.NotesHiltApp",
+    rootPackage = "com.notes.app",
+    originatingRoot = "com.notes.app.NotesHiltApp",
+    originatingRootPackage = "com.notes.app",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "NotesHiltApp",
+    originatingRootSimpleNames = "NotesHiltApp"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_notes_app_NotesHiltApp {
+}
